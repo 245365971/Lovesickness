678f90f81b8e2fa28eccf11e0f932c5cc0b69508
hui <245365971@qq.com>
2015-07-27 22:34:58 +0800

    second commit

---
+++ Lovesickness/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lovesickness
+//
+//  Created by w gq on 15/7/27.
+//  Copyright (c) 2015年 JHH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'Lovesickness' do
+   pod 'FMDB', '~> 2.5'
+end
+
+target 'LovesicknessTests' do
+
+end
+
